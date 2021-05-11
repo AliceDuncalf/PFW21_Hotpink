aa085bead6d45c9a68843490ec6d6200411a3e74
mercieakerblom <mercie.akerblom@hotmail.com>
2021-05-11 16:33:51 +0200

    fått fram rätt antal knappar till kategorierna

--- main/mainScript.js
+++ main/mainScript.js
@@ -6,12 +6,34 @@ function DOMcategoryDiv() {
     categoryDiv.classList.add("categories");
     document.querySelector("#HittaProgram").append(categoryDiv);
     document.querySelector("#HittaProgram").append(programBox);
+
+    
+    let categoryName = FIELDS.map(obj => obj.name);
+    console.log(categoryName);
+    
+    // for (i = 0; i < categoryName.length; i++) {
+    //     categoryDiv.innerHTML += '<button id="selectBox"></button>';
+    //   }
+
+      categoryName.forEach(category => {
+        let button = document.createElement("button");
+        button.classList.add("selectBox")
+        document.querySelector(".categories").append(button);    
+    });
+
+    
 };
 
 DOMcategoryDiv();
 
-console.log(FIELDS);
 
-let c = CITIES.map(obj => obj.name);
-console.log(c);
+
+// let categoryName = FIELDS.map(obj => obj.name);
+// console.log(categoryName);
+ 
+
+// categoryName.forEach(element => {
+//     let div = document.createElement("div");
+//     div.classList.add("hej");
+// });
 
