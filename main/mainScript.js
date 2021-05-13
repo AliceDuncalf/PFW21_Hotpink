2b4c43994c7a8938326245673cb77592733ac1a0
mercieakerblom <mercie.akerblom@hotmail.com>
2021-05-13 18:35:17 +0200

    får upp alla program + rätt universitet

--- main/mainScript.js
+++ main/mainScript.js
@@ -7,24 +7,42 @@ function DOMcategoryDiv() {
     document.querySelector("#HittaProgram").append(categoryDiv);
     document.querySelector("#HittaProgram").append(programBox);
 
-    let categoryName = FIELDS.map(obj => obj.name);
+    let categoryName = FIELDS.map(obj => obj);
+    console.log(categoryName);
     
       categoryName.forEach(category => {
 
         let div = document.createElement("div");
-        div.innerHTML = `<button id="#${category}"></button><div>${category}</div`
-        document.getElementById(`${category}`).addEventListener("click", () => {
-          PROGRAMMES.filter(programme => {
-            if (programme.subjectID === category.id)
-            
-            document.querySelector(".programbox").innerHTML = `
-              ${programme.name}
-           `;
-
-          });
-        });
         div.classList.add("categoryBox")
         document.querySelector(".categories").append(div);
+        div.innerHTML = `<button id="${category.name}"></button><div>${category.name}</div`
+        div.querySelector(`#${category.name}`).addEventListener("click", () => {
+
+          let FilterArray = PROGRAMMES.filter(programme => programme.subjectID === category.id);
+          FilterArray.filter(obj => obj.name);
+          console.log(FilterArray);
+
+          FilterArray.forEach(program => {
+            
+          //console.log(program.name);
+           let programmeContentDiv = document.createElement("div");
+           programmeContentDiv.classList.add("programmeContentDiv")
+            programmeContentDiv.innerHTML = `
+              <div id="programmeName">${program.name}</div>
+              <div id="programmeInfo">
+
+                <div>${GetUniversityNameFromProgramme(program.universityID)}</div>
+                <div>LAND</div>
+                <div> ${GetCityFromUniverityID(program.universityID)}</div>
+                
+              </div>
+            `;
+            document.querySelector(".programbox").append(programmeContentDiv);
+            
+          });
+          
+        });
+        
     });
 
 };
@@ -32,9 +50,54 @@ DOMcategoryDiv();
 
 
 
+//HITTAR NAMNET PÅ DE UNIVERSITET SOM PROGRAMME ÄR PÅ
+function GetUniversityNameFromProgramme(id) {
+  return UNIVERSITIES.find(univerity => univerity.id === id).name;
+};
+
+//Hittar namnet på den staden som programmet är i
+function GetCityFromUniverityID(id) {
+
+  return CITIES.find(city => city.id === id).name;
+};
 
 
 
+function GetCountryFromCityID() {
+  let cityID = UNIVERSITIES.map(uni => uni.cityID) 
+  console.log(cityID);
+    
+};
+
+//clear
+function clearAll(){
+  document.querySelector("programbox").innerHTML = "";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(document.getElementById(`${category}`) != null)  
 
 
 
