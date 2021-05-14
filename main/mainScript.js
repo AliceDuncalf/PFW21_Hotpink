9ccc5651f28bfd49ae466474b8eb743df802f612
AliceDuncalf <aliceduncalf@hotmail.se>
2021-05-14 11:08:52 +0200

    Changed JS, added to index and CSS

--- main/mainScript.js
+++ main/mainScript.js
@@ -1,116 +1,152 @@
 "use strict";
 
+//få ut bild och info till land (språk, visa)
+//inte kunna klicka på ett ämne och få en till div utan tömma och lägga ny
+//få ut kommentarer till stad och program
+//sortera stad/uni/program i bokstavsordning 
+//kolla över namn på variablar så att allt är tydligt
 
-console.log("hej")
-/*let data = {filtername, titel, id};
-console.log(data);*/
 
-//få fram namn och sorterar 
-function getCountryNames (allCountries) {
-    allCountries = COUNTRIES.map(obj => obj.name);
-    allCountries.sort(); //sortera ordentligt
-    return allCountries;
-}
-getCountryNames();
 
-function getCityNames (allCitites) {
-    allCitites = CITIES.map(obj => obj.name);
-    allCitites.sort(); //sortera ordentligt
-    return allCitites;
-}
-getCityNames();
+//globala variablar
 
-function getUniversityNames (allUniversities) {
-    allUniversities = UNIVERSITIES.map(obj => obj.name);
-    allUniversities.sort(); //sortera ordentligt
-    return allUniversities;
-}
-getUniversityNames();
+//select element i HTML
+let selectCountries = document.getElementById("selectCountries"); //borde dessa vara globala variablar? 
+let selectCities = document.getElementById("selectCities");
+let selectUniversities = document.getElementById("selectUniversities");
+let selectProgrammes = document.getElementById("selectProgrammes");
 
-function getProgramNames (allProgrammes) {
-    allProgrammes = PROGRAMMES.map(obj => obj.name);
-    allProgrammes.sort(); //sortera ordentligt
-    return allProgrammes;
-}
-getProgramNames();
 
-//filtrera ut stad utifrån land 
-
-function filterOptions(cityID){
-    let countryID = COUNTRIES.map(obj => obj.id);
-    cityID = CITIES.map(obj => obj.countryID);
-
-    if (cityID === countryID) {
-        return cityID;
-    }
-    console.log(countryID);
-    console.log(cityID);
-
-}
-
-filterOptions()
-
-//lägger in namnen i select elementet (går kanske att göra smartare?)
-function getContentToSelect(titel) {
-    let selectCountries = document.getElementById("selectCountries");
-    let selectCities = document.getElementById("selectCities");
-    let selectUniversities = document.getElementById("selectUniversities");
-    let selectProgrammes = document.getElementById("selectProgrammes");
-
-    COUNTRIES.forEach(allCountries => {
-        titel = allCountries.name;
-        let selectOption = document.createElement("option");
-        selectCountries.append(selectOption);
-        selectOption.classList.add("countriesOptions")
-        selectOption.innerHTML = `${titel}`
+function getCities(countryid){
+    let cities = [];
+     CITIES.forEach(city => {
+        if(city.countryID == countryid) {
+            cities.push(city);
+        }
     })
-
-    CITIES.forEach(allCitites => {
-        titel = allCitites.name;
-        let selectOption = document.createElement("option");
-        selectCities.append(selectOption);
-        selectOption.classList.add(``)
-        selectOption.innerHTML = `${filterOptions()}`
-    })
-
-    UNIVERSITIES.forEach(allUniversities => {
-        titel = allUniversities.name;
-        let selectOption = document.createElement("option");
-        selectUniversities.append(selectOption);
-        selectOption.classList.add("universitiesOptions")
-        selectOption.innerHTML = `${titel}`
-    })
-
-    PROGRAMMES.forEach(allProgrammes => {
-        titel = allProgrammes.name;
-        let selectOption = document.createElement("option");
-        selectProgrammes.append(selectOption);
-        selectOption.classList.add("programmesOptions")
-        selectOption.innerHTML = `${titel}`
-    })
+    return cities;
 }
 
-getContentToSelect();
+function get_Country(countryid) {
+    let country = {};
+    country.cities = getCities(countryid);
+    return country;
+}
+console.log(get_Country(1));
 
 
 
 
+// funktion med landets namn och id
+function getCountries() {
+    //Delar upp land och skapar options för varje, lägger till ett klick-event på valt land-id
+    COUNTRIES.forEach(country => { 
+        let countryname = country.name;
+        let countryOption = document.createElement("OPTION")
+        selectCountries.append(countryOption);
+        countryOption.innerHTML = `${countryname}
+                                    `;
+     
+            countryOption.addEventListener("click", (clicked) => {
+            console.log(clicked);
+            let countryFilter = COUNTRIES.filter(obj => obj.name == countryname);  
+            let chosenCountryID = countryFilter.map(obj => obj.id); 
+            console.log(chosenCountryID);
+            
+            //skapar divar för de landet som är valt med all infomartion
+            let countryContent = document.createElement("div");
+            countryContent.classList.add("contentWrapper");
+            document.getElementById("results").append(countryContent);
+            countryContent.innerHTML = `<h3>${countryOption.value}</h3>
+                                          
+                                        <img src ="filer/Images/${country.imagesNormal[0]}">
+                                        <div>${country.text}</div>
+                                        <div>VISA:${country.visa}</div>
+                                        `; //fixa så flagga allt läggs till här (flagga pch språk), ändra visa värdet?
+            if(clicked === true) {
+                console.log("hej");
+            }
+
+            //I det klick-eventet kommer de städer upp som matchar med de landet som är valt
+            CITIES.forEach(city => {
+                if(chosenCountryID == city.countryID) {
+                    let cityName = city.name;
+                    let cityOption = document.createElement("OPTION");
+                    selectCities.append(cityOption);
+                    cityOption.innerHTML = `${cityName}
+                                            `;                        
+                //skapar ett klick-event där vald stad får all info    
+                cityOption.addEventListener("click", () => {
+                    let cityContent = document.createElement("div");
+                    document.getElementById("results").append(cityContent);
+                    cityContent.classList.add("contentWrapper");
+                    cityContent.innerHTML = `<h3>${city.name}</h3>
+                                            <div>${city.sun}</div>
+                                            <img src ="filer/Images/${city.imagesNormal[1]}">
+                                            <div>${city.text}</div>
+                                            <div>KOMMENTARER</div>`
+                    
+                    //när stad är vald så kommer universitet som matchar stadens id                        
+                    UNIVERSITIES.forEach(uni => {
+                        if(city.id == uni.cityID) {
+                            let uniName = uni.name;
+                            let uniOption = document.createElement("OPTION");
+                            selectUniversities.append(uniOption);
+                            uniOption.innerHTML = `${uniName}`;
+                            
+                            //skapar ett klick-event där vald universitet får all info
+                            uniOption.addEventListener("click", () => {
+                                let uniContent = document.createElement("div");
+                                document.getElementById("results").append(uniContent);
+                                uniContent.classList.add("contentWrapper");
+                                uniContent.innerHTML = `<h3>${uni.name}</h3>
+                                                        <div>KLUBBAR</div>
+                                                        <div></div>`;
+
+                                //när universitet är vald så kommer program som matchar universitetets id    
+                                PROGRAMMES.forEach(program => {
+                                    if(uni.id == program.universityID) {
+                                        let programName = program.name;
+                                        let programOption = document.createElement("OPTION");
+                                        selectProgrammes.append(programOption);
+                                        programOption.innerHTML = `${programName}`;
+                                        
+                                        //klick-event som skapar div med info om programmet som är valt
+                                        programOption.addEventListener("click", () => {
+                                            let programContent = document.createElement("div");
+                                            document.getElementById("results").append(programContent);
+                                            programContent.classList.add("contentWrapper");
+
+                                            /*COMMENTS_PROGRAMME.forEach(program => {
+                                                programAlias = program.alias;
+                                                programText = program.text;
+
+                                            })*/
+                                            programContent.innerHTML = `<h3>${program.name}</h3>
+                                                        <div>ENTRY GRADES: ${program.entryGrades}</div>
+                                                        <div>SUCESS RATE: ${program.successRate}</div>
+                                                        <div>PROGRAM LEVEL: ${program.level}</div>
+                                                        <div>EXCHANGE STUDENTS: ${program.exchangeStudents}</div>
+                                                        <div>LOCAL STUDENTS: ${program.localStudents}</div>
+                                                        <div>KOMMENTARER</div>`;
 
 
-
-
-
-
-
-
-function countryContent() {
-    let countryWrapper = document.createElement("div");
-    document.querySelector("main").append(countryWrapper);
-    document.querySelector(".countriesOptions").addEventListener("click", ()=> {
-        countryWrapper.innerHTML = `<h1>${selectOption.titel}</h1>`
+                                        })
+                                    }
+                                })
+                            })
+                        }
+                    })
+                })
+            } 
+            })           
+            
+        })
     })
-    
-
+ 
 }
 
-countryContent();
+getCountries();
+
+
+   
